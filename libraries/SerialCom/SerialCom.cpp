df602e6530df4f22fb117bd6432730fe40dfeb21
Alan Churichi <achurichi@gmail.com>
2020-07-15 21:14:35 +0100

    duplicate code removed

--- libraries/SerialCom/SerialCom.cpp
+++ libraries/SerialCom/SerialCom.cpp
@@ -48,27 +48,6 @@ String SerialCom::read()
         }
     }
 
-    if (Serial.available() > 0) 
-    {
-        while (Serial.available() > 0)
-        {
-            data_byte = Serial.read();
-            if (char(data_byte) == '_') // Next byte is crc
-                is_crc = true;
-            else
-            {
-                if (is_crc) // Read crc
-                    crc_code += char(data_byte);  
-                else // Read data
-                {
-                    crc.update(data_byte);
-                    msg += char(data_byte);  
-                }
-            }
-        }
-    }
-
-
     crc_code = crc_code.substring(0, crc_code.length() - 1); // Remove '\n' at the end
     if (crc_code == String(crc.finalize()))
         return msg;
