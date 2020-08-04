c9dc75df63f79c8cc7b41ea47b9de9d3c54f0635
Alan Churichi <achurichi@gmail.com>
2020-08-04 19:04:52 +0100

    StaticJsonDocument changed size from 400 to 300

--- src/arduino/main.ino
+++ src/arduino/main.ino
@@ -7,8 +7,8 @@
 SerialCom arduino_serial;
 
 data myData; // data structure
-StaticJsonDocument<400> doc_read; // Readed JSON document
-// StaticJsonDocument<400> doc_send; // Sended JSON document
+StaticJsonDocument<300> doc_read; // Readed JSON document
+// StaticJsonDocument<300> doc_send; // Sended JSON document
 
 LiquidCrystal_I2C lcd(0x3F, 16, 2); // set the LCD address to 0x3F for a 16 chars and 2 line display
 
